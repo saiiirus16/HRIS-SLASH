a6eef4f358d2651a967ab0ed17a98c5c89d3bb2a
Chscodes <124598664+Chscodes@users.noreply.github.com>
2023-04-03 16:41:32 +0800

    revisions

--- Data Controller/Department/insertcode.php
+++ Data Controller/Department/insertcode.php
@@ -3,29 +3,46 @@
      include '../../config.php';
 // Connect to the MySQL database
 
-// Prepare the SQL statement
-$sql = "INSERT INTO dept_tb (`col_deptname`)
-        VALUES (?)";
+$dept_name = $_POST['name_dept'];
 
-// Sanitize the data
 
-$emp = mysqli_real_escape_string($conn, $_POST['name_dept'] . ' ' . 'Department');
+$result_dept = mysqli_query($conn, " SELECT
+            *  
+        FROM
+            dept_tb
+        WHERE col_deptname = '$dept_name'");
 
-// Bind the values to the prepared statement
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, 's',$emp);
+        if(mysqli_num_rows($result_dept) > 0) {
+            $row__dept = mysqli_fetch_assoc($result_dept);
+            header("Location: ../../Department.php?error=You cannot add a department name that is already exist");
+          } 
+          else{
 
-// Execute the statement and check for errors
-if (mysqli_stmt_execute($stmt)) {
-    header("Location: ../../Department.php?msg=Successfully Added");
-   
-} else {
-    echo "Error inserting data: " . mysqli_error($conn);
-}
+                // Prepare the SQL statement
+                $sql = "INSERT INTO dept_tb (`col_deptname`)
+                        VALUES (?)";
 
-// Close the statement and the connection
-mysqli_stmt_close($stmt);
-mysqli_close($conn);
+                // Sanitize the data
+
+                $emp = mysqli_real_escape_string($conn, $_POST['name_dept']);
+
+                // Bind the values to the prepared statement
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, 's',$emp);
+
+                // Execute the statement and check for errors
+                if (mysqli_stmt_execute($stmt)) {
+                    header("Location: ../../Department.php?msg=Successfully Added");
+                
+                } else {
+                    echo "Error inserting data: " . mysqli_error($conn);
+                }
+ 
+                // Close the statement and the connection
+                mysqli_stmt_close($stmt);
+                mysqli_close($conn);
+
+          }
 
 
 
