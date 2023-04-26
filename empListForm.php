5509c52a45804f7444630aabf8039c445ab5165a
Gerard Martin <gerardmartin670@gmail.com>
2023-04-26 12:01:15 +0800

    Fourth Commit

--- empListForm.php
+++ empListForm.php
@@ -223,7 +223,7 @@
              
                                                      $options = "";
                                                      while ($rows = mysqli_fetch_assoc($results)) {
-                                                         $options .= "<option value=' ". $rows['id'] . "'>" .$rows['position'].  "</option>";
+                                                         $options .= "<option value=' ". $rows['position'] . "'>" .$rows['position'].  "</option>";
                                                      }
                                                      ?>
              
