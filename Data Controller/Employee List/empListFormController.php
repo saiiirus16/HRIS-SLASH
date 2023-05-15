417fa775389c23f1cc605350d5fee1fe65bc8578
Gerard Martin <gerardmartin670@gmail.com>
2023-05-15 17:05:02 +0800

    9th Commit

--- Data Controller/Employee List/empListFormController.php
+++ Data Controller/Employee List/empListFormController.php
@@ -216,7 +216,7 @@ if ($stmt1->errno) {
     $mail->Body .= '<h3>Your account details:</h3>';
     $mail->Body .= '<p>Username: ' . $username . '</p>';
     $mail->Body .= '<p>Password: ' . $password . '</p>';
-    $mail->Body .= '<p>Click <a href="http://192.168.0.107:8080/hris/login.php">here</a> to access the website.</p>';
+    $mail->Body .= '<p>Click <a href="http://localhost:8080/hris/empChangePassword.php">here</a> to change your preferred password and to access the website.</p>';
   
     $mail->send();
   }
